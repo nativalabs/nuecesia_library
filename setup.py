1fa82ebbf9199e01ac5d828c2ba8fd029a1e27d3
gabriel-nativa <153530389+gabriel-nativa@users.noreply.github.com>
2024-12-22 23:39:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nuecesia_library",
+    name="nuecesia-library",
     version="0.1.1",
     packages=find_packages(),
     install_requires=[],  # Add dependencies here if needed
